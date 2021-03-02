da08c6ef462dc408b341fb73ffa8a16d3f7b08b4
Vladimir <vovikdoc@gmail.com>
2021-03-02 12:37:45 +0700

    Реализация Model

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Timers;
 using static System.Console;
 
 namespace ConsoleMVVMWatch
@@ -14,7 +15,18 @@ namespace ConsoleMVVMWatch
 
     internal class Model
     {
+        private Timer _timer;
+        public event Action<DateTime> TimeChanged;
 
+        public Model()
+        {
+            _timer = new Timer(1000);
+            _timer.Elapsed += TimerOnElapsed;
+            _timer.Start();
+        }
+
+        private void TimerOnElapsed(object sender, ElapsedEventArgs e)
+            => TimeChanged?.Invoke(e.SignalTime);
     }
 
     internal class View
