1e3b18b8bc014da561d0b2c2699acc7b9884a126
Vladimir <vovikdoc@gmail.com>
2021-03-03 22:13:47 +0700

    Плавный вывод

--- src/Program.cs
+++ src/Program.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Timers;
+using System.Windows.Input;
 using ConsoleMVVMWatch.Annotations;
 using static System.Console;
 
@@ -22,14 +23,13 @@ namespace ConsoleMVVMWatch
 
     internal class Model
     {
-        private Timer _timer;
         public event Action<DateTime> TimeChanged;
 
         public Model()
         {
-            _timer = new Timer(1000);
-            _timer.Elapsed += TimerOnElapsed;
-            _timer.Start();
+            var timer = new Timer(1000);
+            timer.Elapsed += TimerOnElapsed;
+            timer.Start();
         }
 
         private void TimerOnElapsed(object sender, ElapsedEventArgs e)
@@ -38,7 +38,8 @@ namespace ConsoleMVVMWatch
 
     internal sealed class ViewModel : INotifyPropertyChanged
     {
-        private string _time = "00:00:00";
+        public const string Midnight = "00:00:00";
+        private string _time = Midnight;
         public string Time
         {
             get => _time;
@@ -54,7 +55,8 @@ namespace ConsoleMVVMWatch
         private void ModelOnTimeChanged(DateTime obj)
             => Time = obj.ToLongTimeString();
 
-        public event PropertyChangedEventHandler? PropertyChanged;
+        [CanBeNull]
+        public event PropertyChangedEventHandler PropertyChanged;
 
         [NotifyPropertyChangedInvocator]
         private void OnPropertyChanged(
@@ -69,7 +71,7 @@ namespace ConsoleMVVMWatch
     {
         private object DataContext { get; }
 
-        private string _text;
+        private string _text, _oldText;
         public string Text
         {
             get => _text;
@@ -77,6 +79,7 @@ namespace ConsoleMVVMWatch
             {
                 if (string.Equals(_text, value))
                     return;
+                _oldText = _text?.Remove(_text.Length - 1);
                 _text = value;
                 Update();
             }
@@ -84,9 +87,21 @@ namespace ConsoleMVVMWatch
 
         private void Update()
         {
-            Clear();
+            var buf = _oldText ?? string.Empty;
 
-            Write(Text);
+            while (buf.Length > 0 && !Text.StartsWith(buf))
+                buf = buf.Remove(buf.Length - 1);
+
+            if (buf.Length == 0)
+            {
+                Clear();
+                Write(Text);
+            }
+            else
+            {
+                Write(new string('\b', Text.Length - buf.Length));
+                Write(Text.Remove(0, buf.Length));
+            }
         }
 
         public View(ViewModel dataContext)
@@ -117,7 +132,7 @@ namespace ConsoleMVVMWatch
         public void Show()
         {
             Update();
-            ReadLine();
+            ReadKey();
         }
     }
 
